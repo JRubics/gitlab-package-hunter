e7f9b0c7ed7132de785e113b44f5752fd234f1ad
Jelena Dokic <jelena.dokic@uns.ac.rs>
2021-08-04 00:57:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+node -v
 # # Trust the falcosecurity GPG key, configure the apt repository, and update the package list
 # curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | sudo apt-key add -
 # echo "deb https://download.falco.org/packages/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
@@ -71,8 +72,8 @@ cd package-hunter
 cp falco/falco_rules.local.yaml /etc/falco/ && service falco restart
 npm ci
 
-// copy files server.crt, client.crt, and client.key that have been created
-// during the configuration of Falco's gRPC API in this directory
+# copy files server.crt, client.crt, and client.key that have been created
+# during the configuration of Falco's gRPC API in this directory
 cp ~/server.crt ~/client.crt ~/client.key .
 
 DEBUG=pkgs* node src/server.js
