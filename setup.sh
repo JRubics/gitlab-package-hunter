4c932f6498f917a037383fcad50bef44ecaa8e94
Jelena Dokic <jelena.dokic@uns.ac.rs>
2021-08-04 00:38:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,53 +7,16 @@ sudo apt-get update -y
 sudo apt-get -y install linux-headers-$(uname -r)
 
 # Install Falco
-sudo apt-get -y install dkms build-essential apt-transport-https ca-certificates gnupg lsb-release falco=0.23.0
+sudo apt-get -y install falco=0.23.0
 
-# Generate certificates for Falco.
-## Create a RANDFILE for the root user.
-openssl rand -writerand /root/.rnd
-## Generate valid CA.
-openssl genrsa -passout pass:1234 -des3 -out ca.key 4096
-openssl req -passin pass:1234 -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=SP/ST=Italy/L=Ornavasso/O=Test/OU=Test/CN=Root CA"
-## Generate valid Server Key/Cert.
-openssl genrsa -passout pass:1234 -des3 -out server.key 4096
-openssl req -passin pass:1234 -new -key server.key -out server.csr -subj  "/C=SP/ST=Italy/L=Ornavasso/O=Test/OU=Server/CN=localhost"
-openssl x509 -req -passin pass:1234 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
-## Remove passphrase from the Server Key.
-openssl rsa -passin pass:1234 -in server.key -out server.key
-## Generate valid Client Key/Cert.
-openssl genrsa -passout pass:1234 -des3 -out client.key 4096
-openssl req -passin pass:1234 -new -key client.key -out client.csr -subj  "/C=SP/ST=Italy/L=Ornavasso/O=Test/OU=Client/CN=localhost"
-openssl x509 -passin pass:1234 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
-## Remove passphrase from Client Key.
-openssl rsa -passin pass:1234 -in client.key -out client.key
-## Move files to correct location.
-mkdir /etc/falco/certs
-mv server.key /etc/falco/certs/
-mv server.crt /etc/falco/certs/
-mv ca.crt /etc/falco/certs/
-mv client.key /etc/falco/certs/
-mv client.crt /etc/falco/certs/
-mv client.csr /etc/falco/certs/
-## Make files world-readable.
-chmod +r /etc/falco/certs/*
+git clone https://gitlab.com/gitlab-org/security-products/package-hunter.git
+cd package-hunter
+cp falco/falco_rules.local.yaml /etc/falco/ && service falco restart
+npm ci
 
-# Move Falco configuration file to correct location.
-cp /vagrant/falco/falco.yaml /etc/falco/
+// copy files server.crt, client.crt, and client.key that have been created
+// during the configuration of Falco's gRPC API in this directory
+cp ~/server.crt ~/client.crt ~/client.key .
 
-# Install Falco driver
-falco-driver-loader
+DEBUG=pkgs* node src/server.js
 
-# Start Falco
-service falco start
-
-# git clone https://gitlab.com/gitlab-org/security-products/package-hunter.git
-# cd package-hunter
-# cp falco/falco_rules.local.yaml /etc/falco/ && service falco restart
-# npm ci
-
-# // copy files server.crt, client.crt, and client.key that have been created
-# // during the configuration of Falco's gRPC API in this directory
-# cp ~/server.crt ~/client.crt ~/client.key .
-
-# DEBUG=pkgs* node src/server.js
