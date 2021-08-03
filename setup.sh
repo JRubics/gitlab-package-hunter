914b8bf9f4dc722076289d631a240083779bd498
Jelena Dokic <jelena.dokic@uns.ac.rs>
2021-08-04 00:17:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-echo "Goodbye"
+docker -v
+falco -v
+# git clone https://gitlab.com/gitlab-org/security-products/package-hunter.git
+# cd package-hunter
+# cp falco/falco_rules.local.yaml /etc/falco/ && service falco restart
+# npm ci
+
+# // copy files server.crt, client.crt, and client.key that have been created
+# // during the configuration of Falco's gRPC API in this directory
+# cp ~/server.crt ~/client.crt ~/client.key .
+
+# DEBUG=pkgs* node src/server.js
