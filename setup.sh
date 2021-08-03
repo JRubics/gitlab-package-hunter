46effcdbb0cf8c7e9b1928ea638ce1ec44b5fd5d
Jelena Dokic <jelena.dokic@uns.ac.rs>
2021-08-04 00:30:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-whoami
-
 # Trust the falcosecurity GPG key, configure the apt repository, and update the package list
-curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
+sudo curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
 sudo echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
 sudo apt-get update -y
 
